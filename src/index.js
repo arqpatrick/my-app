d7ff99401c41b8e16c8786a9323328ca9eaab683
Patrick <patrickkottwitz@gmail.com>
2021-08-22 19:21:52 -0300

    criação de template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import App from './App'
+import './index.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#cccccc',
+    }
+  },
+})
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
