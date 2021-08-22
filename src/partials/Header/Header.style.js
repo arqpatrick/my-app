5404dd734125e179b958ef5148af5025f44304c3
Patrick <patrickkottwitz@gmail.com>
2021-08-22 18:42:40 -0300

    adicionado material-ui

---
+++ src/partials/Header/Header.style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/styles'
+
+// JSS
+const useStyles = makeStyles(() => ({
+  title: {
+    flexGrow: 1,
+    marginLeft: 10,
+  }
+}))
+
+export default useStyles
